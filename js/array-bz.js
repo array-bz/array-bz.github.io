4726c7f0fd8de40f3f78efe8a85b0d46409a0ad5
Sam Taylor <sjltaylor@gmail.com>
2015-12-28 18:11:29 +0000

    javascript page title tricks

---
+++ js/array-bz.js
@@ -0,0 +1,7 @@
+window.addEventListener('focus', function() {
+  document.title = "ARRAY.BZ";
+});
+
+window.addEventListener('blur', function() {
+  document.title = "ARRAY.BZ: Don't hang up!";
+});
