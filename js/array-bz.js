e1896dc8fdd1b33ca9ccd5ea3fee3416ddd4a5f3
Sam Taylor <sjltaylor@gmail.com>
2015-12-29 18:37:51 +0000

    mark unread pages

--- js/array-bz.js
+++ js/array-bz.js
@@ -5,3 +5,19 @@ window.addEventListener('focus', function() {
 window.addEventListener('blur', function() {
   document.title = "ARRAY.BZ: Don't hang up!";
 });
+
+$(document).ready(function () {
+  array_bz.init();
+});
+
+array_bz = {
+  init: function () {
+    localStorage.setItem(window.location, "read");
+
+    $("a.readable").each(function() {
+      if (localStorage.getItem(this.href) !== "read") {
+        $(this).addClass("unread");
+      }
+    });
+  }
+};
